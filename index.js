7c9a261cfda3a28bbdcfddd6b29107d481efa783
Sara Muntean <munteansr@gmail.com>
2018-09-10 14:46:23 -0400

    First draft of a React blog

---
+++ index.js
@@ -0,0 +1,45 @@
+let h = React.createElement;
+// h(tag, {props}, [children]);
+
+let posts = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "eum et est occaecati",
+    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+  },
+]
+
+let postList = posts.map(post =>
+  h('ul', null, [
+    h('h1', {className: 'post-title'}, post.title),
+    h('li', {className: 'post-author'}, `Posted by: User ${post.userId}`),
+    h('li', {className: 'post-body'}, post.body),
+  ])
+);
+
+let page = h('div', null, [
+  h('h1', {className: 'header'}, ['Sleeping Belle']),
+  h('ul', {className: 'post-list'}, postList),
+  ]
+);
+
+ReactDOM.render(page, document.querySelector('.react-root'));
